e53cb171ff58bf2ae4139eef91bd0163865f9b63
madhusudhan <madhu6699sudhan@gmail.com>
2021-11-26 07:29:29 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/Home.js
@@ -0,0 +1,332 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+     <div class="header">
+      <div class="pagecenter">
+         <div class="header1"id="home1">
+            <p><i class="fas fa-shopping-basket"></i>Groco</p>
+         </div>
+         <div class="header2">
+            <ul class="menu_list">
+               <li><a href="#home1">Home</a></li>
+               <li><a href="#features1">Features</a></li>
+               <li><a href="#products1">Products</a></li>
+               <li><a href="#categories1">Categories</a></li>
+               <li><a href="#review1">Review</a></li>
+               <li><a href="#blogs1">Blogs</a></li>
+            </ul>
+         </div>
+         <div class="header3">
+            <ul class="icon_list">
+               <li><i class="fa fa-search"></i></li>
+               <li><i class='fas fa-shopping-cart'></i></li>
+               <li><i class="fa fa-user"></i></li>
+            </ul>
+         </div>
+      </div>
+   </div>
+   <div class="hero">
+      <div class="hero1">
+         <h2>fresh and <span class="org">organic</span> products for your</h2>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam libero nostrum veniam facere tempore nisi.</p>
+         <button class="bt">Shop Now</button>
+      </div>
+   </div>
+   <div class="feature_parent" id="features1">
+      <div class="pagecenter">
+         <div class="feature">
+            <h2>Our<span class="fea"> Features</span></h2>
+            <div class="feature_child1">
+               <img src="images/feature-img-1.png" alt="image here"/>
+               <h1>fresh and organic</h1>
+               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, earum!</p>
+               <button class="btn">Read More</button>
+            </div>
+            <div class="feature_child2">
+               <img src="images/feature-img-2.png" alt="image here"/>
+               <h1>free delivery</h1>
+               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, earum!</p>
+               <button class="btn">Read More</button>
+            </div>
+            <div class="feature_child3">
+               <img src="images/feature-img-3.png" alt="image here"/>
+               <h1>easy payments</h1>
+               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, earum!</p>
+               <button class="btn">Read More</button>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="product_parent" id="products1">
+      <div class="pagecenter">
+         <div class="product">
+            <h2>Our<span class="pro"> Products</span></h2>
+            <div class="product_child1">
+               <img src="images/product-1.png" alt="image here"/>
+               <h1>fresh orange</h1>
+               <p>$4.99/- -10.99/-</p>
+               <ul class="star_list">
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class='fas fa-star'></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fa fa-star-half-alt"></i></li>
+               </ul>
+               <button class="btn">Add To Cart</button>
+            </div>
+            <div class="product_child2">
+               <img src="images/product-2.png" alt="image here"/>
+               <h1>fresh onion</h1>
+               <p>$4.99/- -10.99/-</p>
+               <ul class="star_list">
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class='fas fa-star'></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fa fa-star-half-alt"></i></li>
+               </ul>
+               <button class="btn">Add To Cart</button>
+            </div>
+            <div class="product_child3">
+               <img src="images/product-3.png" alt="image here"/>
+               <h1>fresh meat</h1>
+               <p>$4.99/- -10.99/-</p>
+               <ul class="star_list">
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class='fas fa-star'></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fa fa-star-half-alt"></i></li>
+               </ul>
+               <button class="btn">Add To Cart</button>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="product1_parent">
+      <div class="pagecenter">
+         <div class="product1">
+            <div class="product1_child1">
+               <img src="images/product-7.png" alt="image here"/>
+               <h1>fresh carrot</h1>
+               <p>$4.99/- -10.99/-</p>
+               <ul class="star_list">
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class='fas fa-star'></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fa fa-star-half-alt"></i></li>
+               </ul>
+               <button class="btn">Add To Cart</button>
+            </div>
+            <div class="product1_child2">
+               <img src="images/product-8.png" alt="image here"/>
+               <h1>fresh lemon</h1>
+               <p>$4.99/- -10.99/-</p>
+               <ul class="star_list">
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class='fas fa-star'></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fa fa-star-half-alt"></i></li>
+               </ul>
+               <button class="btn">Add To Cart</button>
+            </div>
+            <div class="product1_child3">
+               <img src="images/product-5.png" alt="image here"/>
+               <h1>fresh potato</h1>
+               <p>$4.99/- -10.99/-</p>
+               <ul class="star_list">
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class='fas fa-star'></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fa fa-star-half-alt"></i></li>
+               </ul>
+               <button class="btn">Add To Cart</button>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="categories_parent" id="categories1">
+      <div class="pagecenter">
+         <div class="categories">
+            <h2>Products<span class="pro">Categories</span></h2>
+            <div class="categories_child1">
+               <img src="images/cat-1.png" alt="image here"/>
+               <h1>vegetables</h1>
+               <p>upto 45% off</p>
+               <button class="btn">Shop Now</button>
+            </div>
+            <div class="categories_child2">
+               <img src="images/cat-2.png" alt="image here"/>
+               <h1>Fresh Fruits</h1>
+               <p>upto 45% off</p>
+               <button class="btn">Shop Now</button>
+            </div>
+            <div class="categories_child3">
+               <img src="images/cat-3.png" alt="image here"/>
+               <h1>Dairy Products</h1>
+               <p>upto 45% off</p>
+               <button class="btn">Shop Now</button>
+            </div>
+            <div class="categories_child4">
+               <img src="images/cat-4.png" alt="image here"/>
+               <h1>Fresh Meat</h1>
+               <p>upto 45% off</p>
+               <button class="btn">Shop Now</button>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="review_parent" id="review1">
+      <div class="pagecenter">
+         <div class="review">
+            <h2>Customer's<span class="fea">Review </span></h2>
+            <div class="review_child1">
+               <img src="images/pic-1.png" alt="image here"/>
+               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde sunt fugiat dolore ipsum id est maxime ad tempore quasi tenetur.</p>
+               <h3>Jhon Doe</h3>
+               <ul class="star_list">
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class='fas fa-star'></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fa fa-star-half-alt"></i></li>
+               </ul>
+            </div>
+            <div class="review_child2">
+               <img src="images/pic-2.png" alt="image here"/>
+               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde sunt fugiat dolore ipsum id est maxime ad tempore quasi tenetur.</p>
+               <h3>Jhon Doe</h3>
+               <ul class="star_list">
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class='fas fa-star'></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fa fa-star-half-alt"></i></li>
+               </ul>
+            </div>
+            <div class="review_child3">
+               <img src="images/pic-3.png" alt="image here"/>
+               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde sunt fugiat dolore ipsum id est maxime ad tempore quasi tenetur.</p>
+               <h3>Jhon Doe</h3>
+               <ul class="star_list">
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class='fas fa-star'></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fas fa-star"></i></li>
+                  <li><i class="fa fa-star-half-alt"></i></li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="blogs_parent" id="blogs1">
+      <div class="pagecenter">
+         <div class="blogs">
+            <h2>Our<span class="fea">Blogs </span></h2>
+            <div class="blogs_child1">
+               <img src="images/blog-1.jpg" alt="image here"/>
+               <div class="all">
+                  <div class="inside">
+                     <div class="inside1">
+                        <p><span class="no1"><i class="fas fa-user"></i></span>By User</p>
+                     </div>
+                     <div class="inside2">
+                        <p><span class="no1"><i class="fas fa-calendar"></i></span>1st May,2021</p>
+                     </div>
+                  </div>
+                  <h3>fresh and organic vegetables and fruits</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, expedita.</p>
+                  <button class="btn1">Read More</button>
+               </div>
+            </div>
+            <div class="blogs_child2">
+               <img src="images/blog-2.jpg" alt="image here"/>
+               <div class="all">
+                  <div class="inside">
+                     <div class="inside1">
+                        <p><span class="no1"><i class="fas fa-user"></i></span>By User</p>
+                     </div>
+                     <div class="inside2">
+                        <p><span class="no1"><i class="fas fa-calendar"></i></span>1st May,2021</p>
+                     </div>
+                  </div>
+                  <h3>fresh and organic vegetables and fruits</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, expedita.</p>
+                  <button class="btn1">Read More</button>
+               </div>
+            </div>
+            <div class="blogs_child3">
+               <img src="images/blog-3.jpg" alt="image here"/>
+               <div class="all">
+                  <div class="inside">
+                     <div class="inside1">
+                        <p><span class="no1"><i class="fas fa-user"></i></span>By User</p>
+                     </div>
+                     <div class="inside2">
+                        <p><span class="no1"><i class="fas fa-calendar"></i></span>1st May,2021</p>
+                     </div>
+                  </div>
+                  <h3>fresh and organic vegetables and fruits</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, expedita.</p>
+                  <button class="btn1">Read More</button>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="footer_parent">
+      <div class="pagecenter">
+         <div class="footer_big">
+            <div class="footer1">
+               <h3>Groco<i class="fas fa-shopping-basket"></i></h3>
+               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, saepe.</p>
+               <ul class="list1">
+                  <li><i class="fab fa-facebook-f"></i></li>
+                  <li><i class='fab fa-twitter'></i></li>
+                  <li><i class="fab fa-instagram"></i></li>
+                  <li><i class="fab fa-linkedin"></i></li>
+               </ul>
+            </div>
+            <div class="footer2">
+               <h3>contact info</h3>
+               <ul class="list2">
+                  <li><i class="fas fa-phone"></i><span class="new">+123-456-7890</span></li>
+                  <li><i class="fas fa-phone"></i><span class="new">+111-222-3333</span></li>
+                  <li><i class="fas fa-envelope"></i><span class="new">shaikhanas@gmail.com</span></li>
+                  <li><i class="fas fa-map-marker-alt"></i><span class="new">Mumbai,India-400104</span></li>
+               </ul>
+            </div>
+            <div class="footer3">
+               <h3>quick links</h3>
+               <ul class="list3">
+                  <li><a href="#"><i class="fas fa-arrow-right"></i><span class="new">Home</span></a></li>
+                  <li><a href="#features1"><i class="fas fa-arrow-right"></i><span class="new">Features</span></a></li>
+                  <li><a href="#products1"><i class="fas fa-arrow-right"></i><span class="new">Products</span></a></li>
+                  <li><a href="#categories1"><i class="fas fa-arrow-right"></i><span class="new">categories</span></a></li>
+                  <li><a href="#review1"><i class="fas fa-arrow-right"></i><span class="new">Review</span></a></li>
+                  <li><a href="#blogs1"><i class="fas fa-arrow-right"></i><span class="new">Blogs</span></a></li>
+               </ul>
+            </div>
+            <div class="footer4">
+               <h3>newsletter</h3>
+               <p>subscribe for latest updates</p>
+               <form><input class="fm" type="text" placeholder="Your Email"/></form>
+               <button class="btn2">Subscribe</button><br/>
+               <img src="images/payment.png" alt="image here"/>
+            </div>
+         </div>
+         <div class="footer5">
+            <p>created by<span class="foot"> Madhusudhan p  </span>| all rights reserved </p>
+         </div>
+      </div>
+   </div> 
+        </div>
+    )
+    }  
+
+export default Home;
